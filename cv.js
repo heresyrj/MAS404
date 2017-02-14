f4218bbbcfb0b9b43562b889d676cc660598bec4
Jian Ruan <ruanjianusa@gmail.com>
2017-02-14 13:34:05 -0500

    update manual filter

--- cv.js
+++ cv.js
@@ -1,9 +1,15 @@
 let Clarifai = require("clarifai");
 let Account = require("./account.json");
-
+let Categories = require("./categories.json");
 let app = new Clarifai.App(`${Account.clientId}`, `${Account.clientSecret}`);
 
 // predict the contents of an image by passing in a path (url)
+
+let categoryfilter = name => {
+  for (let category in Categories) {
+    return Categories[category].includes(name);
+  }
+};
 
 module.exports.getPrediction = url => {
   return new Promise((resolve, reject) => {
@@ -11,9 +17,11 @@ module.exports.getPrediction = url => {
     app.models
       .predict(Clarifai.FOOD_MODEL, url)
       .then(response => {
-        let concepts = response.outputs[0].data.concepts
-        let rawPredicts = concepts.map(item => obj = {name: item.name, value: item.value} )
-        
+        let concepts = response.outputs[0].data.concepts;
+        let rawPredicts = concepts
+          .map(item => obj = { name: item.name, value: item.value })
+          .filter(item => categoryfilter(item.name));
+
         resolve(rawPredicts);
         //reject
       })
